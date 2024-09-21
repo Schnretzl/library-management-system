60d7a86bf658588d5d86d345ec7d3785473ddff5
Schnretzl <wrhendley@gmail.com>
2024-09-21 11:50:24 -0500

    Add get_valid_user_id

--- user_menu.py
+++ user_menu.py
@@ -5,7 +5,7 @@ def add_user(users):
     users.append(User(name))
     
 def view_user_details(users):
-    user_id = input("Enter the ID of the user: ")
+    user_id = get_valid_user_id(users)
     find_user_index = user_index(users, user_id)
     if find_user_index is None:
         print("User not found.")
@@ -18,13 +18,24 @@ def view_user_details(users):
         print(f"Author: {book.author}")
         print(f"Genre: {book.genre}")
         print(f"Publication Date: {book.publication_date}")
+    print()
 
 def display_users(users):
-    pass
-
+    for user in users:
+        print(f"Name: {user.name}")
+        print(f"ID: {user.id}")
+        print()
+        
 def user_index(users, user_id):
     #Return the index of the user, or None if the user is not found
     for index, user in enumerate(users):
         if user.id == user_id:
             return index
     return None
+
+def get_valid_user_id():
+    while True:
+        user_id = input("Enter the ID of the user: ")
+        if user_id.isdigit():
+            return int(user_id)
+        print("Invalid user ID. Please try again.")
