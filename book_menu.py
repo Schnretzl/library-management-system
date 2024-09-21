cbd2872343e93d94869f6b50d682df8b3279ac31
Schnretzl <wrhendley@gmail.com>
2024-09-21 09:39:55 -0500

    Finish return_book

--- book_menu.py
+++ book_menu.py
@@ -28,7 +28,26 @@ def borrow_book(books, users):
         return False
         
 def return_book(books, users):
-    pass
+    user_id = input("Enter your user ID: ")
+    user_index = user_menu.user_index(users, user_id)
+    if user_index is None:
+        print("User not found.")
+        return False
+    book_title = input("Enter the title of the book you want to return: ")
+    book_title_index = book_index(books, book_title)
+    user_book_index = book_index(users[user_index].borrowed_books, book_title)
+    if book_title_index is None:
+        print("Book not found.")
+        return False
+    elif user_book_index is None:
+        print("Book not borrowed.")
+        return False
+    elif books[book_title_index].return_book():
+        users[user_index].borrowed_books.remove(books[book_title_index])
+        return True
+    else:
+        print("Book is already available.")
+        return False
 
 def book_index(books, title):
     #Return the index of the book, or None if the book is not found
